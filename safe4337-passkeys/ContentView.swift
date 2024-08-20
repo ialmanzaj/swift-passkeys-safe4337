f6180a205a407229d0b1d5c68ddf293fd4b85193
Isaac Almanza <isaac.almanza19@gmail.com>
2024-08-20 16:48:31 -0500

    Initial Commit

---
+++ safe4337-passkeys/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  safe4337-passkeys
+//
+//  Created by Isaac Almanza on 08/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ safe4337-passkeys/safe4337_passkeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  safe4337_passkeysApp.swift
+//  safe4337-passkeys
+//
+//  Created by Isaac Almanza on 08/20/24.
+//
+
+import SwiftUI
+
+@main
+struct safe4337_passkeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
